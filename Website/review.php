75cb05582014c6bb55ad085130dd536e4210ecde
TobCar <TobCar@users.noreply.github.com>
2018-04-23 12:11:20 -0700

    Replace font awesome icon with stylish button

--- Website/review.php
+++ Website/review.php
@@ -3,8 +3,6 @@
 
   <head>
       <?php echo file_get_contents("./global/head-tags.html") ?>
-      <!-- Used by the edit buttons. -->
-      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
       <!-- Prevent this page from being indexed by search engines. -->
       <meta name="robots" content="noindex">
   </head>
@@ -31,97 +29,97 @@
             <tr>
                 <th class="review-question-column-m">Question</th>
                 <th class="review-answer-column">Answer</th>
-                <th class="icon-h">Edit</th>
+                <th>Edit</th>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][0]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][0]) ){ echo $_SESSION["committed_answers"][0]; } ?></td>
-                <td class="icon"><button name="jump_to" value="0" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="0" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][1]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][1]) ){ echo $_SESSION["committed_answers"][1]; } ?></td>
-                <td class="icon"><button name="jump_to" value="1" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="1" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][2]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][2]) ){ echo $_SESSION["committed_answers"][2]; } ?></td>
-                <td class="icon"><button name="jump_to" value="2" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="2" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][3]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][3]) ){ echo $_SESSION["committed_answers"][3]; } ?></td>
-                <td class="icon"><button name="jump_to" value="3" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="3" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][4]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][4]) ){ echo $_SESSION["committed_answers"][4]; } ?></td>
-                <td class="icon"><button name="jump_to" value="4" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="4" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][5]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][5]) ){ echo $_SESSION["committed_answers"][5]; } ?></td>
-                <td class="icon"><button name="jump_to" value="5" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="5" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][6]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][6]) ){ echo $_SESSION["committed_answers"][6]; } ?></td>
-                <td class="icon"><button name="jump_to" value="6" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="6" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][7]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][7]) ){ echo $_SESSION["committed_answers"][7]; } ?></td>
-                <td class="icon"><button name="jump_to" value="7" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="7" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][8]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][8]) ){ echo $_SESSION["committed_answers"][8]; } ?></td>
-                <td class="icon"><button name="jump_to" value="8" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="8" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][9]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][9]) ){ echo $_SESSION["committed_answers"][9]; } ?></td>
-                <td class="icon"><button name="jump_to" value="9" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="9" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][10]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][10]) ){ echo $_SESSION["committed_answers"][10]; } ?></td>
-                <td class="icon"><button name="jump_to" value="10" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="10" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][11]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][11]) ){ echo $_SESSION["committed_answers"][11]; } ?></td>
-                <td class="icon"><button name="jump_to" value="11" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="11" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][12]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][12]) ){ echo $_SESSION["committed_answers"][12]; } ?></td>
-                <td class="icon"><button name="jump_to" value="12" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="12" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][13]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][13]) ){ echo $_SESSION["committed_answers"][13]; } ?></td>
-                <td class="icon"><button name="jump_to" value="13" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="13" class="review-btn">Edit</button></td>
             </tr>
 
             <tr>
                 <td><?php echo $_SESSION["mc_questions"][14]?></td>
                 <td class="answer"><?php if( isset($_SESSION["committed_answers"][14]) ){ echo $_SESSION["committed_answers"][14]; } ?></td>
-                <td class="icon"><button name="jump_to" value="14" class="fa fa-pencil"></button></td>
+                <td><button name="jump_to" value="14" class="review-btn">Edit</button></td>
             </tr>
 
         </table>
@@ -147,55 +145,55 @@
           <tr>
               <td><?php echo $_SESSION["fr_questions"][0]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][15]) ){ echo $_SESSION["committed_answers"][15]; } ?></td>
-              <td class="icon"><button name="jump_to" value="15" class="fa fa-pencil"></button></td>
+              <td><button name="jump_to" value="15" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][1]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][16]) ){ echo $_SESSION["committed_answers"][16]; } ?></td>
-              <td class="icon"><button name="jump_to" value="16" class="fa fa-pencil"></button></td>
+              <td><button name="jump_to" value="16" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][2]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][17]) ){ echo $_SESSION["committed_answers"][17]; } ?></td>
-              <td class="icon"><button name="jump_to" value="17" class="fa fa-pencil"></button></td>
+              <td><button name="jump_to" value="17" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][3]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][18]) ){ echo $_SESSION["committed_answers"][15]; } ?></td>
-              <td class="icon"><button name="jump_to" value="18" class="fas pencil-alt"></button></td>
+              <td><button name="jump_to" value="18" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][4]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][19]) ){ echo $_SESSION["committed_answers"][16]; } ?></td>
-              <td class="icon"><button name="jump_to" value="19" class="fas pencil-alt"></button></td>
+              <td><button name="jump_to" value="19" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][5]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][20]) ){ echo $_SESSION["committed_answers"][17]; } ?></td>
-              <td class="icon"><button name="jump_to" value="20" class="fas pencil-alt"></button></td>
+              <td><button name="jump_to" value="20" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][6]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][21]) ){ echo $_SESSION["committed_answers"][15]; } ?></td>
-              <td class="icon"><button name="jump_to" value="21" class="fas pencil-alt"></button></td>
+              <td><button name="jump_to" value="21" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][7]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][22]) ){ echo $_SESSION["committed_answers"][16]; } ?></td>
-              <td class="icon"><button name="jump_to" value="22" class="fas pencil-alt"></button></td>
+              <td><button name="jump_to" value="22" class="review-btn">Edit</button></td>
           </tr>
 
           <tr>
               <td><?php echo $_SESSION["fr_questions"][8]?></td>
               <td class="answer"><?php if( isset($_SESSION["committed_answers"][23]) ){ echo $_SESSION["committed_answers"][17]; } ?></td>
-              <td class="icon"><button name="jump_to" value="23" class="fas pencil-alt"></button></td>
+              <td><button name="jump_to" value="23" class="review-btn">Edit</button></td>
           </tr>
 
       </table>
@@ -212,7 +210,7 @@
       <!-- Submit Button -->
 
     <form action="https://www.bcco-inovaca.org/finish" method="post" onsubmit="return confirm('You will not be able to change any of your answers. Press OK if you are sure you are done.');">
-      <input type="submit" class="btn btn-default" style="margin:0 auto; display:block; width:160px; margin-bottom: 16px;" name="submit" value="Submit">
+      <input type="submit" class="review-btn" style="margin:0 auto; display:block; width:160px; margin-bottom: 16px;" name="submit" value="Submit">
       <input type="hidden" name="finished_willingly" value="true">
     </form>
 
